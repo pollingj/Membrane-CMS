39c3bd49498cac463aca30df7674c7157800282b
John Polling <john@theusualsuspect.com>
2009-03-25 11:55:23 +0000

    Removed some folders that missed the purge!

--- Membrane.Plugins.Blog/DTOs/PostDetailsDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace Membrane.Plugins.Blog.DTOs
-{
-    public class PostDetailsDTO
-    {
-        public string Title { get; set; }
-        public string Body { get; set; }
-    }
-}
--- Membrane.Plugins.Blog/Services/IBlogService.cs
+++
@@ -1,9 +0,0 @@
-using Membrane.Plugins.Blog.DTOs;
-
-namespace Membrane.Plugins.Blog.Services
-{
-    public interface IBlogService
-    {
-        PostDetailsDTO RetrievePostByUri(string uri);
-    }
-}
