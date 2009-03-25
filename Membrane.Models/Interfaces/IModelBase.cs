63c1f7f26cc52f9e3c93f24fa16b8b1967e5a029
John Polling <john@theusualsuspect.com>
2009-03-25 11:54:57 +0000

    Removed some folders that missed the purge!

--- Membrane.Models/Interfaces/IModelBase.cs
+++
@@ -1,7 +0,0 @@
-namespace Membrane.Models.Interfaces
-{
-    public interface IModelBase
-    {
-        int Id { get; }
-    }
-}
--- Membrane.Models/Interfaces/IPropertyBag.cs
+++
@@ -1,9 +0,0 @@
-namespace Membrane.Models.Interfaces
-{
-    public interface IPropertyBag : IModelBase
-    {
-        string Name { get; set; }
-        // TODO: Add the query info (or FindAll() etc idea)
-        // need to consider DetachedCriteria
-    }
-}
