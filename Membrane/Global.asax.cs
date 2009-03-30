0c19fe360c19aed01455a2fbaae1209900af0a81
John Polling <john@theusualsuspect.com>
2009-03-30 15:35:38 +0000

    put in the latest versions of jQuery and jQuery Validation (uncompressed).
    They are uncompressed as I've added the JSCombine viewcomponent along with the YUICompressor patch. (http://support.castleproject.org/projects/MR/issues/view/MR-ISSUE-457)
    
    Updated the routing rules to ignore the "MonoRail" path used by the compression script.
    
    May need to look at turning JS obfuscation setting on.
    
    Updated the default.spark to use the compression viewcomponent.

--- Membrane/Global.asax.cs
+++ Membrane/Global.asax.cs
@@ -1,5 +1,7 @@
 ﻿using System.Reflection;
 using Castle.MonoRail.Framework.Routing;
+using Castle.MonoRail.Framework.Services;
+using Castle.MonoRail.Framework.ViewComponents;
 using Membrane.Commons.Web.MonoRail;
 using Membrane.Core.Mappers;
 using Membrane.Core.Services;
@@ -21,14 +23,19 @@ namespace Membrane
 		public override void RegisterApplicationComponents()
 		{
 			container.AddComponent<IAuthenticationService, AuthenticationService>();
+
+			container.AddComponent<IScriptBuilder, YuiScriptBuilder>();
+			container.AddComponent("JSCombine", typeof(CombineJSViewComponent));
 		}
 
 		public override void RegisterRoutes(RoutingEngine rules)
 		{
 			rules.Add(new PatternRoute("[controller]/[action]")
-				.DefaultForArea().IsEmpty
-				.DefaultForController().Is("Home")
-				.DefaultForAction().Is("Index"));
+			          	.DefaultForArea().IsEmpty
+			          	.DefaultForController().Is("Home")
+			          	.DefaultForAction().Is("Index")
+			          	.Restrict("controller").AnythingBut("MonoRail"));
+			
 		}
 
     }
