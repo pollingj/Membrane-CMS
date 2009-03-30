8a3884cb8a67e158eca5e4556e880b6a294d143a
John Polling <john@theusualsuspect.com>
2009-03-30 07:18:01 +0000

    Started to look at moving the automapping stuff into a central place

---
+++ Membrane.Commons/IMapper.cs
@@ -0,0 +1,7 @@
+namespace Membrane.Commons
+{
+	public interface IMapper
+	{
+		
+	}
+}
---
+++ Membrane.Core/Mappers/AutoMapperConfiguration.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using Membrane.Entities;
+
+namespace Membrane.Core.Mappers
+{
+	public class AutoMapperConfiguration
+	{
+		public static void Configure()
+		{
+			/*var mappers = Mapper.CreateMap<MembraneUser, MembraneUser>();
+			IObjectMapper mapper = new AutoMapper.o
+			var conf = new Configuration(mappers)*/
+		}
+	}
+}
