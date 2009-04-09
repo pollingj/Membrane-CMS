28e209449616a1401a35762bbe1d8c1351f6ded1
John Polling <john@theusualsuspect.com>
2009-04-09 16:22:07 +0000

    Forgot to make the properties virtual

--- Membrane.Entities/NavigationNode.cs
+++ Membrane.Entities/NavigationNode.cs
@@ -5,6 +5,7 @@ namespace Membrane.Entities
 {
 	public class NavigationNode : IEntity
 	{
-		public Guid Id { get; set; }
+		public virtual Guid Id { get; set; }
+		public virtual string Name { get; set; }
 	}
 }
