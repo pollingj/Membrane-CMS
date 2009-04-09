ba6ed7bcd2f43d6b61106fd77ebefa82b24dca77
John Polling <john@theusualsuspect.com>
2009-04-09 13:05:52 +0000

    Commented the CRUDService

--- Membrane.Commons/CRUD/Services/CRUDService.cs
+++ Membrane.Commons/CRUD/Services/CRUDService.cs
@@ -8,6 +8,11 @@ using Membrane.Commons.Persistence.Exceptions;
 
 namespace Membrane.Commons.CRUD.Services
 {
+	/// <summary>
+	/// Basic CRUDService that is used as standard in the CRUDController.  It can be overwritten if required.
+	/// </summary>
+	/// <typeparam name="TDto">The DTO type</typeparam>
+	/// <typeparam name="TEntity">The Entity type</typeparam>
 	public class CRUDService<TDto, TEntity> : ICRUDService<TDto, TEntity> 
 		where TDto : IDTO
 		where TEntity : IEntity
@@ -21,12 +26,25 @@ namespace Membrane.Commons.CRUD.Services
 			RegisterMappings();
 		}
 
+		/// <summary>
+		/// Does two basic mappings
+		///  - TEntity -> TDto
+		///  - TDto -> TEntity
+		/// 
+		/// Can be overwritten for more specific mappings
+		/// </summary>
 		public virtual void RegisterMappings()
 		{
 			Mapper.CreateMap<TEntity, TDto>();
 			Mapper.CreateMap<TDto, TEntity>();
 		}
 
+		/// <summary>
+		/// Get the Items using Pagination
+		/// </summary>
+		/// <param name="currentPage">The page number</param>
+		/// <param name="pageSize">The size of a page</param>
+		/// <returns>A List of Dto objects</returns>
 		public virtual IList<TDto> GetPagedItems(int currentPage, int pageSize)
 		{
 			var skip = 0;
@@ -39,6 +57,11 @@ namespace Membrane.Commons.CRUD.Services
 			return Mapper.Map<ICollection<TEntity>, IList<TDto>>(items);
 		}
 
+		/// <summary>
+		/// Gets an individual item
+		/// </summary>
+		/// <param name="id">The id of the item</param>
+		/// <returns>The item</returns>
 		public virtual TDto GetItem(Guid id)
 		{
 			var group = repository.FindById(id);
@@ -46,6 +69,11 @@ namespace Membrane.Commons.CRUD.Services
 			return Mapper.Map<TEntity, TDto>(group);
 		}
 
+		/// <summary>
+		/// Creates (saves) a new item
+		/// </summary>
+		/// <param name="item">The item that needs to be saved</param>
+		/// <returns>The Guid of the new object</returns>
 		public virtual Guid Create(TDto item)
 		{
 			var id = item.Id = Guid.NewGuid();
@@ -62,6 +90,11 @@ namespace Membrane.Commons.CRUD.Services
 			return id;
 		}
 
+		/// <summary>
+		/// Updates an item
+		/// </summary>
+		/// <param name="item">The item that needs to be updated</param>
+		/// <returns>Has the item been updated successfully?</returns>
 		public virtual bool Update(TDto item)
 		{
 			var success = true;
@@ -77,6 +110,11 @@ namespace Membrane.Commons.CRUD.Services
 			return success;
 		}
 
+		/// <summary>
+		/// Deletes an item
+		/// </summary>
+		/// <param name="id">The id of the item that needs to be deleted</param>
+		/// <returns>Has the item been deleted successfully?</returns>
 		public virtual bool Delete(Guid id)
 		{
 			var success = true;
