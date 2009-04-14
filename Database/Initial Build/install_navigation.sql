cadc2c52f1c8b17e709b43e55eeaf136b5711589
John Polling <john@theusualsuspect.com>
2009-04-14 16:38:45 +0000

    Updated the install navigation sql

--- Database/Initial Build/install_navigation.sql
+++ Database/Initial Build/install_navigation.sql
@@ -3,5 +3,22 @@ create table NavigationType (
 	Name nvarchar(255) not null
 )
 
+create table NavigationNode (
+	Id UniqueIdentifier primary key clustered not null,
+	Type_Id UniqueIdentifier not null,
+	Parent_Id UniqueIdentifier,
+	Name nvarchar(255) not null,
+	Title nvarchar(255) not null,
+	AccessKey char(1),
+	ExternalUrl nvarchar(255),
+	
+)
+
+alter table NavigationNode add constraint FK_NavigationNode_NavigationType foreign key (Type_Id) references NavigationType
+alter table NavigationNode add constraint FK_NavigationNode_ParentNavigationNode foreign key (Parent_Id) references NavigationNode
+
 insert into NavigationType (Id, Name)
 values (NewId(), 'Primary Navigation')
+
+insert into NavigationNode (Id, Type_Id, Name, Title, AccessKey)
+select NewId(), (Select Id From NavigationType WHERE Name = 'Primary Navigation'), 'Home',  'Visit the site home page', '1'
