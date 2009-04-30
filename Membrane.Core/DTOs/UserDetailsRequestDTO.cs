97279fe2b13791437f0161058293e8bee69ce3e0
John Polling <john@theusualsuspect.com>
2009-04-30 11:47:28 +0000

    Created UserDetails controller with tests.
    The controller allows users to update their own details.

---
+++ Membrane.Core/DTOs/UserDetailsRequestDTO.cs
@@ -0,0 +1,18 @@
+using System;
+using Castle.Components.Validator;
+
+namespace Membrane.Core.DTOs
+{
+	public class UserDetailsRequestDTO
+	{
+		[ValidateGuid(false)]
+		public Guid Id { get; set; }
+		[ValidateNonEmpty]
+		public string Name { get; set; }
+		[ValidateEmail]
+		public string Email { get; set; }
+		public string Password { get; set; }
+		[ValidateSameAs("Password")]
+		public string ConfirmPassword { get; set; }
+	}
+}
---
+++ Membrane.Core/DTOs/UserDetailsResponseDTO.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Membrane.Core.DTOs
+{
+	public class UserDetailsResponseDTO
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+	}
+}
