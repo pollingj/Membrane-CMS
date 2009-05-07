a1bc6b60ba1c289d8b4217ec51e2bb6dc0a6047e
John Polling <john@theusualsuspect.com>
2009-05-07 19:34:30 +0000

    missed the install_users.sql from a previous checkin

--- Database/Initial Build/install_users.sql
+++ Database/Initial Build/install_users.sql
@@ -8,7 +8,9 @@ create table MembraneUser (
 	Type_Id UniqueIdentifier not null,
 	Group_Id UniqueIdentifier,
 	Username nvarchar(255) not null,
-	Password nvarchar(255) not null
+	Password nvarchar(255) not null,
+	[Name] nvarchar(255) not null,
+	Email nvarchar(255) not null
 )
 
 create table UserGroup (
@@ -26,10 +28,10 @@ values (NewId(), 'User')
 insert into MembraneUserType (Id, Type)
 values (NewId(), 'Site')
 
-insert into MembraneUser(Id, Type_Id, Username, Password)
-select NewId(), (Select Id From MembraneUserType WHERE Type = 'Administrator'),  'johnpolling', 'B1-09-F3-BB-BC-24-4E-B8-24-41-91-7E-D0-6D-61-8B-90-08-DD-09-B3-BE-FD-1B-5E-07-39-4C-70-6A-8B-B9-80-B1-D7-78-5E-59-76-EC-04-9B-46-DF-5F-13-26-AF-5A-2E-A6-D1-03-FD-07-C9-53-85-FF-AB-0C-AC-BC-86'
-insert into MembraneUser(Id, Type_Id, Username, Password)
-select NewId(), (Select Id From MembraneUserType WHERE Type = 'User'),  'andypike', 'B1-09-F3-BB-BC-24-4E-B8-24-41-91-7E-D0-6D-61-8B-90-08-DD-09-B3-BE-FD-1B-5E-07-39-4C-70-6A-8B-B9-80-B1-D7-78-5E-59-76-EC-04-9B-46-DF-5F-13-26-AF-5A-2E-A6-D1-03-FD-07-C9-53-85-FF-AB-0C-AC-BC-86'
+insert into MembraneUser(Id, Type_Id, Username, Password, [Name], Email)
+select NewId(), (Select Id From MembraneUserType WHERE Type = 'Administrator'),  'johnpolling', 'B1-09-F3-BB-BC-24-4E-B8-24-41-91-7E-D0-6D-61-8B-90-08-DD-09-B3-BE-FD-1B-5E-07-39-4C-70-6A-8B-B9-80-B1-D7-78-5E-59-76-EC-04-9B-46-DF-5F-13-26-AF-5A-2E-A6-D1-03-FD-07-C9-53-85-FF-AB-0C-AC-BC-86', 'John Polling', 'john@theusualsuspect.com'
+insert into MembraneUser(Id, Type_Id, Username, Password, [Name], Email)
+select NewId(), (Select Id From MembraneUserType WHERE Type = 'User'),  'andypike', 'B1-09-F3-BB-BC-24-4E-B8-24-41-91-7E-D0-6D-61-8B-90-08-DD-09-B3-BE-FD-1B-5E-07-39-4C-70-6A-8B-B9-80-B1-D7-78-5E-59-76-EC-04-9B-46-DF-5F-13-26-AF-5A-2E-A6-D1-03-FD-07-C9-53-85-FF-AB-0C-AC-BC-86', 'Andy Pike', 'andy@test.com'
 
 insert into UserGroup (Id, Name)
 values (NewId(), 'Blogging Group')
