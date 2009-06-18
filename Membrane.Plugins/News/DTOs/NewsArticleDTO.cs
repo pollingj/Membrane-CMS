926cf3ed340d7b1a05fb0f380f4ed52803bb0e3f
John Polling <john@theusualsuspect.com>
2009-06-18 15:52:22 +0000

    Dropped in the wymeditor.  Need to configure it.
    Added the TextEditor fieldtype for the NewsArticleDTO

--- Membrane.Plugins/News/DTOs/NewsArticleDTO.cs
+++ Membrane.Plugins/News/DTOs/NewsArticleDTO.cs
@@ -1,10 +1,13 @@
 using System;
+using Membrane.Commons.FormGeneration.Attributes;
+using Membrane.Commons.FormGeneration.Enums;
 using Membrane.Commons.Plugin.DTOs;
 
 namespace Membrane.Plugins.News.DTOs
 {
 	public class NewsArticleDTO : BaseVersionedDTO
 	{
+		[FormFieldType(FieldType.TextEditor)]
 		public string Story { get; set; }
 		public DateTime CreationDate { get; set; }
 	}
