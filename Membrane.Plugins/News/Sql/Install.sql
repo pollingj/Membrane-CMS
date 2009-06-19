294de76b2bafbac303a870ee8359df1ed8affd69
John Polling <john@theusualsuspect.com>
2009-06-19 10:34:44 +0000

    Updated the News Install sql to include the versioning fields
    Added in the WYMEditor for TextEditor fields

--- Membrane.Plugins/News/Sql/Install.sql
+++ Membrane.Plugins/News/Sql/Install.sql
@@ -1,6 +1,10 @@
-﻿create table NewsArticle (
+create table NewsArticle (
 	Id UniqueIdentifier primary key clustered not null,
 	Name nvarchar(255) not null,
 	Story text not null,
-	CreationDate DateTime not null
+	CreationDate DateTime not null,
+	Culture_Id UniqueIdentifier not null,
+	Revision int not null,
+	ParentEntity_Id UniqueIdentifier,
+	Published bit not null,
 )
